5d450ac3236adac1b7709038b15b9e4730a0ef68
Mat Sadler <mat@redstudio.co.uk>
2013-02-28 15:33:53 +0000

    changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -81,40 +81,49 @@
 				</div>
 			</div>
 		</div>
+		<div id="contact">
+			<div  class="row map">
+				<div class="container">
+					<div class="span5 box_wrapp">
+						<div class="box_cont">
+							<div class="head">
+								<h6>Contact</h6>
+							</div>
+							<ul class="street">
+								<li>
+									2301 East Lamar Blvd. Suite 140.
+								</li>
+								<li>
+									City, Arlington. United States,
+								</li>
+								<li>
+									Zip Code, TX 76006.
+								</li>
+								<li class="icon icontop">
+									<span class="contacticos ico1"></span>
+									<span class="text">1 817 274 2933</span>
+								</li>
+								<li class="icon">
+									<span class="contacticos ico2"></span>
+									<a class="text" href="#">bootstrap@twitter.com</a>
+								</li>
+							</ul>
+
+							<div class="head headbottom">
+								<h6>Work with us</h6>
+							</div>
+							<p>
+								We’ve prepared a simple project planner to get to know you and your project better.
+							</p>
+
+							<a href="#" class="btn">Let's get started</a>
+						</div>
+					</div>
+				</div>
+				<iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/?ie=UTF8&amp;ll=64.089157,-21.816616&amp;spn=0.045157,0.15398&amp;t=m&amp;z=13&amp;output=embed"></iframe>
+			</div>
+		</div>
 		
-		 <div id="contact" class="row map">
-            <div class="container">
-                <div class="span5 box_wrapp">
-                    <div class="box_cont">
-                        <div class="head">
-                            <h6>Contact</h6>
-                        </div>
-                        <ul class="street">
-                            <li>2301 East Lamar Blvd. Suite 140.</li>
-                            <li>City, Arlington. United States,</li>
-                            <li>Zip Code, TX 76006.</li>
-                            <li class="icon icontop">
-                                <span class="contacticos ico1"></span>
-                                <span class="text">1 817 274 2933</span>
-                            </li>
-                            <li class="icon">
-                                <span class="contacticos ico2"></span>
-                                <a class="text" href="#">bootstrap@twitter.com</a>
-                            </li>
-                        </ul>
-
-                        <div class="head headbottom">
-                            <h6>Work with us</h6>
-                        </div>
-                        <p>We’ve prepared a simple project planner to get to know you and your project better.</p>
-
-                        <a href="#" class="btn">Let's get started</a>
-                    </div>
-                </div>
-            </div>
-            <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/?ie=UTF8&amp;ll=64.089157,-21.816616&amp;spn=0.045157,0.15398&amp;t=m&amp;z=13&amp;output=embed"></iframe>
-        </div>
-    </div>
 
 		<div id="aboutus">
 			<div class="container">
@@ -137,7 +146,7 @@
 						</p>
 
 						<p>
-To arrange a viewing get in contact. Either use our contact form, or call Matthew Skidmore on 07973 129285
+							To arrange a viewing get in contact. Either use our contact form, or call Matthew Skidmore on 07973 129285
 						</p>
 
 					</div>
