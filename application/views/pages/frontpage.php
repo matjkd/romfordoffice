a410187c30eaf676fd566ca8b29e0218d4ae9f3c
Mat Sadler <mat@redstudio.co.uk>
2013-03-01 15:21:50 +0000

    text update

--- application/views/pages/frontpage.php
+++ application/views/pages/frontpage.php
@@ -19,7 +19,7 @@
 				</p>
 
 				<p>
-					To arrange a viewing get in contact: Call Matthew Skidmore on 07973 129285
+					For more details or to arrange a viewing call Matthew Skidmore on 07973 129285
 				</p>
 
 			</div>
